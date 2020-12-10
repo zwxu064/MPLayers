cc8b6cf3cbb1d90650e5f94960db33e67749d02b
Zhiwei Xu <u5710355@cecs-039614.cecs.anu.edu.au>
2020-12-10 15:44:43 +1100

    Adding energy with actual connections

--- matlab/main.m
+++ matlab/main.m
@@ -4,11 +4,12 @@ clc;
 
 marker_order = struct('SGM', 1, 'MF', 2, 'ISGMR', 3, 'TRWP', 4, 'TRWS', 5);
 
-img = "tsukuba";
+img = "000002_11";
 markers = {'-+','-o','-*','-.','-x','-s','-d','-^','-v','->','-<','-p','-h'};
 save_dir = fullfile('../experiments', char(img));
 enable_block_minAdir = false;
 checkDir(save_dir);
+force_4_energy_connection = false;
 
 clean_names = {};
 list = [dir(sprintf('%s/*_unary*.mat', save_dir));
@@ -36,6 +37,12 @@ for idx = 1 : n_img
   rho = str2num(split_obj{11 + offset});
   enable_min_a_dir = false;
   
+  if force_4_energy_connection
+    energy_dirs = 4;
+  else
+    energy_dirs = n_dir;
+  end
+  
   if rho == 1
     rho_type = 'loopy';
   else
@@ -60,20 +67,21 @@ for idx = 1 : n_img
     energy.(clean_name) = SparseTRWSEnergyFile(file_path);
   else
     obj = load(file_path);
-    unary_cost = obj.unary;
-    pairwise_cost = obj.label_context;
+    unary_cost = single(obj.unary);
+    pairwise_cost = single(obj.label_context);
     seg_all = obj.seg_all;
     n_disp = size(unary_cost, 3);
 
     for iter = 1 : n_iter
       seg = seg_all(:, :, iter);
       energy.(clean_name)(end+1) = ...
-        CalEnergy(unary_cost, pairwise_cost(:, :, 1), seg + 1);
+        CalEnergy(unary_cost, pairwise_cost(:, :, 1), seg + 1, energy_dirs);
       if iter == 1 || iter == n_iter
         new_save_dir =fullfile(save_dir, sprintf('%d', iter));
         checkDir(new_save_dir);
         file_path = fullfile(new_save_dir, strcat(name, '.png'));
-        imwrite(uint8(single(seg) ./ (n_disp - 1) .* 255), file_path, 'png');
+        imwrite(uint8(single(seg) ./ (n_disp - 1) .* 255), file_path, ...
+            'png');
       end
     end
   end
@@ -128,26 +136,45 @@ else
 end
 
 % =========================================================================
-function energy = CalEnergy(data_cost, pairwise_cost, labeling)
+function energy = CalEnergy(data_cost, pairwise_cost, labeling, num_dirs)
 %CALENERGY Summary of this function goes here
 %   Detailed explanation goes here
 [height, width] = size(labeling);
 energy = 0;
 
+h_steps = [0, 1];
+w_steps = [1, 0];
+
+if num_dirs > 4
+  h_steps = cat(2, h_steps, [1, -1]);
+  w_steps = cat(2, w_steps, [1, 1]);
+end
+
+if num_dirs > 8
+  h_steps = cat(2, h_steps, [2, 1, -1, -2]);
+  w_steps = cat(2, w_steps, [1, 2, 2, 1]);
+end
+
+num_neighs = length(h_steps);
+
 for h = 1 : height
   for w = 1 : width
-    energy = energy + data_cost(h, w, labeling(h, w));
-    
-    if (w < width)
-      xi = labeling(h, w);
-      xj = labeling(h, w + 1);
-      energy = energy + pairwise_cost(xi, xj);
-    end
-    
-    if (h < height)
-      xi = labeling(h, w);
-      xj = labeling(h + 1, w);
-      energy = energy + pairwise_cost(xi, xj);
+    node_i_h = h;
+    node_i_w = w;
+    node_i_label = labeling(node_i_h, node_i_w);
+    energy = energy + data_cost(node_i_h, node_i_w, node_i_label);
+ 
+    for k = 1 : num_neighs
+      h_step = h_steps(k);
+      w_step = w_steps(k);
+      node_j_h = node_i_h + h_step;
+      node_j_w = node_i_w + w_step;
+
+      if node_j_h <= height && node_j_h > 0 ...
+          && node_j_w <= width && node_j_w > 0
+        node_j_label = labeling(node_j_h, node_j_w);
+        energy = energy + pairwise_cost(node_i_label, node_j_label);
+      end
     end
   end
 end
